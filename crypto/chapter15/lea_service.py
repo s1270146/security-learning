3c6c49c9eca60d56b3e10158fd2c68e9f78882e8
s1270146 <s1270146@gmail.com>
2023-06-12 16:16:42 +0900

    md5のサービス作成

--- crypto/chapter15/lea_service.py
+++ crypto/chapter15/lea_service.py
@@ -5,7 +5,10 @@ from binascii import hexlify, unhexlify
 
 SALT = b'secretsalt'
 
+#  ----- Register ----
 print('Register')
+
+# encode は utf-8に変換している
 username = input('user name > ').encode()
 if b'=' in username:
     print('Bye.')
@@ -13,6 +16,32 @@ if b'=' in username:
 
 # ユーザの名前と他の文字列を加えてバイトに変換する
 userdata = b'priv=guest,username=' + username
+
+# SALTとuserdataをmdを使って暗号化
+# hexdigest()で16進形式文字列にする
 h = hashlib.md5(SALT + userdata).hexdigest()
-print('Your username(hex) is ', hexlify(username).decode())
+
+# hexlify は、 バイナリdataの16進表現
+# ソルトとくっつけたuserdataを表示させている
+print('Your userdata(hex) is ', hexlify(userdata).decode())
 print('Your signature is: ', h)
+
+# ---- Login ----
+print('Login')
+
+# unhexlifyで16進数表記を文字列に戻す
+userdata = unhexlify(input('userdata(hex) > '))
+sig = input('signature(hex) > ')
+
+h = hashlib.md5(SALT + userdata).hexdigest()
+if h == sig:
+    print('Login OK!')
+
+    if b'priv=admin' in userdata.split(b','):
+        print('Congratulations! You are admin!')
+    else:
+        print('Sorry, admin only')
+
+else:
+    print('Bye.')
+    sys.exit(1)
