be4430da0121ca6fc75b3e17cd196f3adc24af80
s1270146 <s1270146@gmail.com>
2023-06-15 12:38:42 +0900

    15章

--- crypto/chapter15/lea_md5.py
+++ crypto/chapter15/lea_md5.py
@@ -50,3 +50,46 @@ def md5(msg, IV=(0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476), offset=0):
     gs = [(1, 0), (5, 1), (3, 5), (7, 0),]
     for i in range(len(msg) // 64):
         block = msg[i*64:(i+1)*64]
+        A, B, C, D = A0, B0, C0, D0
+
+        for j in range(64):
+            jj = j // 16
+            F = Fs[jj](B, C, D)
+            g = (gs[jj][0] * j + gs[jj][1]) % 16
+
+            word = int.from_bytes(block[g*4: g*4+4], 'little')
+            F = (Fs[jj](B, C, D) + A + K[j] + word) & 0xffffffff
+            A = D
+            D = C
+            C = B
+            B = B + rol(F, S[j])
+
+        A0 = (A0 + A) & 0xffffffff
+        B0 = (B0 + B) & 0xffffffff
+        C0 = (C0 + C) & 0xffffffff
+        D0 = (D0 + D) & 0xffffffff
+
+    return b''.join(map(lambda x: x.to_bytes(4, byteorder='little'),[A0, B0, C0, D0]))
+
+if __name__ == '__main__':
+    from binascii import hexlify, unhexlify
+    data = input('data > ')
+    h = unhexlify(input('hash > '))
+    add_data = input('additional data > ').encode()
+    prefix_len = int(input('prefix length > '))
+
+    # 1. hashからMD5の内部状態を復元
+    A = int.from_bytes(h[:4], 'little')
+    B = int.from_bytes(h[4:8], 'little')
+    C = int.from_bytes(h[8:12], 'little')
+    D = int.from_bytes(h[12:16], 'little')
+
+    # 2. パディングを計算
+    padded_data = md5_pad(data.encode(), prefix_len)
+
+    # 3. 新しいハッシュを計算
+    new_hash = md5(add_data, (A, B, C, D), len(padded_data) + prefix_len)
+
+    # 4. 新しいハッシュ値を出力
+    print('data: ', hexlify(padded_data + add_data).decode())
+    print('hash: ', hexlify(new_hash).decode())
